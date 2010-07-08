96c848ec15270bd088f8500ed7b7254b58f8b3e3
AlexSnet <me@alexsnet.ru>
2010-07-09 03:19:00 +0400

    Webserver plugin start

--- plugins/webserver_test/main.py
+++ plugins/webserver_test/main.py
@@ -6,36 +6,36 @@ class TestWebserver(Plugin):
     implements(apis.webserver.Server)
     
     name = 'Dummy'
-				
-	# Contorlling server state
-	def getState(self):
-		return 1
-	
-	def stop(self):
-		return 1
-	
-	def start(self):
-		return 1
-	
-	def restart(self):
-		return 1
-		
-	# Configuring
-	def getVirtualHosts(self):
-		return 1
-	
-	def getConfiguration(self):
-		return 1
-	
-	def getModules(self):
-		return 1
-	
-	def getVirtualHost(self, host):
-		return 1
-	
-	def addNewVirtualHost(self, hostname, args):
-		return 1
-	
-	# UI control
-	def getPage(self, pageName ):
-		return 1
+                
+    # Contorlling server state
+    def getState(self):
+        return 1
+    
+    def stop(self):
+        return 1
+    
+    def start(self):
+        return 1
+    
+    def restart(self):
+        return 1
+        
+    # Configuring
+    def getVirtualHosts(self):
+        return 1
+    
+    def getConfiguration(self):
+        return 1
+    
+    def getModules(self):
+        return 1
+    
+    def getVirtualHost(self, host):
+        return 1
+    
+    def addNewVirtualHost(self, hostname, args):
+        return 1
+    
+    # UI control
+    def getPage(self, pageName ):
+        return 1
